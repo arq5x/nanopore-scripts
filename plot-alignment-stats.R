93b81839d8d524c349827fd8dd34c0b10da8b1fa
arq5x <arq5x@virginia.edu>
2014-09-16 06:02:58 -0400

    refine identity calculation

--- plot-alignment-stats.R
+++ plot-alignment-stats.R
@@ -5,11 +5,12 @@ args <- commandArgs(trailingOnly = TRUE)
 mapstat <- read.table(args[1], header=TRUE)
 
 ggplot(mapstat, 
-       aes(x=identity, 
-           y=align_len/(align_len+unalign_len), 
+       aes(x=100*matches/(matches+deletions+insertions+mismatches), 
+           y=(align_len+insertions-deletions)/(align_len+unalign_len-deletions+insertions), 
            colour=read_type)) +
-  xlab("Percent identity (mm+ins+del)/(align_len)") +
-  ylab("Fraction of read aligned") +
-  geom_point(alpha=0.8) + 
-  xlim(0.4,1.0) +
-  theme_bw(base_size=22) 
+  xlab("Percent identity: 100* matches/(matches+deletions+insertions+mismatches)") +
+  ylab("Fraction of read aligned: \n(align_len+ins-del)/(align_len+unalign_len-del+ins)") +
+  geom_point(alpha=0.5) + 
+  xlim(20,100) +
+  ylim(0.0,1.0) +
+  theme_bw(base_size=24)
