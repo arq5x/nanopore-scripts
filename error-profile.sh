d8e5dab571d9acf3cf653996557d9577f9980f45
Aaron Quinlan <aaronquinlan@gmail.com>
2014-09-19 13:13:26 -0400

    Update error-profile.sh

--- error-profile.sh
+++ error-profile.sh
@@ -1,6 +1,6 @@
 
-ONTREADS=/Users/arq5x/Documents/Career/Meetings/2014-GenomeInformatics/ecoli_01/downloads
-GENOME=/Users/arq5x/Documents/Career/Meetings/2014-GenomeInformatics/ecoli.k12.fa
+ONTREADS=/path/to/ONT/fast5/files/
+GENOME=/path/to/reference/genome/in/fasta/format.fa
 STUB="myreads"
 
 # extract high quality reads is FASTA format.
@@ -10,7 +10,7 @@ poretools fasta --high-quality $ONTREADS > $STUB.fa
 lastdb genome $GENOME
 
 # align high quaklity reads to reference genome with LAST
-lastal -q 2 -a 1 -b 1 genome $STUB.fa > $STUB.fa.maf
+lastal -q 1 -a 1 -b 1 genome $STUB.fa > $STUB.fa.maf
 
 # convert the MAF to BAM with complete CIGAR (matches and mismatches)
 python maf-convert.py sam $STUB.fa.maf | \
